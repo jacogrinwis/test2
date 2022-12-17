ab881975d9893eea6cde6bd229d0d15a3b12a99f
Jaco Grinwis <jacogrinwis@gmail.com>
2022-12-17 15:00:13 +0100

    Edit Models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'body'];
+
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 }
